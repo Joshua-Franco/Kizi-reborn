8d235a03acb32920a97d7201fd96530d944bcf27
Joshua Franco <joshuafrancostudio2023@gmail.com>
2026-06-18 03:29:56 -0500

    fix the code so it could be on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { serveGamesPlugin } from './src/plugins/serveGamesPlugin'
 import { ruffleAssetsPlugin } from './src/plugins/ruffleAssetsPlugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Kizi-reborn/',
-  plugins: [react(), tailwindcss(), serveGamesPlugin(), ruffleAssetsPlugin()],
+  plugins: [react(), tailwindcss(), ruffleAssetsPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
